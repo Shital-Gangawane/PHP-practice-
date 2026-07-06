53ed0376fd48eb72191ba24563eb624e147c05d9
Shital Gangawane <shital@example.com>
2026-07-06 23:55:53 +0530

    commit

---
+++ arithmetic_Operator.php
@@ -0,0 +1,32 @@
+<?php
+
+$a=20;
+$b=6;
+
+echo "<h2>Arithmetic Operators in PHP</h2>";
+
+echo "<h3>Addition</h3>";
+echo "Addition of ($a+$b)=".($a+$b); //Addition of two numbers using + operator.
+echo "<br>";
+
+echo "<h3>Subtraction</h3>";
+echo "Subtraction of ($a-$b)=". ($a-$b); //Subtraction of two numbers using - oprator.
+echo "<br>";
+
+echo "<h3>Multiplication</h3>";
+echo "Multiplication of ($a*$b)=". ($a*$b); //Multiplication of two numbers using * operator.
+echo "<br>";
+
+echo "<h3>Division</h3>";
+echo "Division of ($a/$b)=". ($a/$b); //Division of two numbers using / operator.
+echo "<br>";
+
+echo "<h3>Modulus</h3>";
+echo "Modulus of ($a%$b)=". ($a%$b); //Modulus of two numbers using % operator.
+echo "<br>";
+
+echo "<h3>Exponentiation</h3>";
+echo "Exponent of ($a**$b)=". ($a**$b); //Exponentiation of two numbers using ** operator.
+echo "<br>";
+
+?>
---
+++ assignment_operators.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<h2>Assignment Operators in PHP</h2>";
+$a=10; //assigning value 10 to variable a using = operator.
+
+echo "Initial value = $a <br>";
+
+$a += 5; //adding 5 to variable a using += operator.
+echo "After adding 5 = $a <br>";
+
+
+
+?>
