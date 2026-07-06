146a91f586afedab2726a4d7d8e1b96c648f6422
Shital Gangawane <shital@example.com>
2026-07-07 00:04:41 +0530

    assignment_operators.php

--- assignment_operators.php
+++ assignment_operators.php
@@ -8,6 +8,22 @@ echo "Initial value = $a <br>";
 $a += 5; //adding 5 to variable a using += operator.
 echo "After adding 5 = $a <br>";
 
+$a -= 3; //subtracting 3 from variable a using -= operator.
+echo "After subtracting 3 = $a <br>";
+
+$a *=2; //multiplying variable a by 2 using *= operator.
+echo "After multiplying by 2 = $a <br>";
+
+$a /=4; //dividing variable a by 4 using /= operator.
+echo "After dividing by 4 = $a <br>";
+
+$a %=3; //modulus of variable a by 3 using %= operator.
+echo "After modulus by 3 = $a <br>";
+
+$a **=2; //exponent of variable a by 2 using **= opersator.
+echo "After exponent by 2 = $a <br>";
+
+
 
 
 ?>
